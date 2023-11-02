7ce7b5af703ce9ad0a781f9208ae48bbec0474fd
abroudoux <arthur.broudoux@gmail.com>
2023-11-02 07:18:20 +0100

    Create monorepo with Turbo

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-
-
 @Module({
-    imports: [
-        // MongooseModule.forRoot('mongodb://localhost/choosify', { useNewUrlParser : true })
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-
 export class AppModule {}
