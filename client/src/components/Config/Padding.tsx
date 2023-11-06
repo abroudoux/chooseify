f9e4e7fbccdf9e78ce2b04eccb9820cda0c3ccc4
abroudoux <arthur.broudoux@gmail.com>
2023-11-06 21:28:37 +0100

    fix git problem

---
+++ client/src/components/Config/Padding.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from "react";
+
+
+type PaddingProps = {
+    position : string,
+    color : string,
+};
+
+const Padding : FC<PaddingProps> = ( props ) => {
+
+    return (
+
+        <div className={`${props.position}-0 left-0 fixed h-4 w-full bg-${props.color} z-20`}></div>
+
+    )
+};
+
+export default Padding;
