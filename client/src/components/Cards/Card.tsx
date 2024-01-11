f405845934d40267e6e686be8d901ae72b11d7c0
arthur <arthur.broudoux@gmail.com>
2024-01-11 17:45:14 +0100

    Create Postgres DB

--- client/src/components/Cards/Card.tsx
+++ client/src/components/Cards/Card.tsx
@@ -9,7 +9,7 @@ type CardProps = {
 
 const Card : FC<CardProps> = ( props ) => {
     return (
-        <div className=" flex flex-col items-start gap-2">
+        <div className="flex flex-col items-start gap-2">
             <div className="card h-36 w-72 flex-row-center-center border-gradient p-0.5 rounded-lg">
                 <div className="h-full w-72 flex justify-start items-end py-2 px-3 bg-grey-dark rounded-lg">
                     <h3 className="text-base font-normal text-grey-light">
